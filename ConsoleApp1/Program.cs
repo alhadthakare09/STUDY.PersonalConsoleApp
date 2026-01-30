7db6bfbecfb2a904bd0b7dc9a4ae50798d5ffe08
Alhad Avinash Thakare <alhadthakare09@gmail.com>
2026-01-30 16:24:23 +0530

    Revert "Keeping only one messages"
    This reverts commit 3fa636772e46005f74d432ef107f7caee237caf5.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy WELCOME YOU");
+Console.WriteLine("Hello, C# Academy WELCOME YOU"); Console.WriteLine("Hello, C# Academy WELCOME YOU"); Console.WriteLine("Hello, C# Academy WELCOME YOU"); Console.WriteLine("Hello, C# Academy WELCOME YOU"); Console.WriteLine("Hello, C# Academy WELCOME YOU");
